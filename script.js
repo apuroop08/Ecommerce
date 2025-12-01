56a0df0d5ae4bd929403f864b8b9091cd4a9ca5f
apuroop08 <apuroopkumar8143@gmail.com>
2025-12-01 18:14:23 +0530

    Added shop,blog and productDetails pages

--- script.js
+++ script.js
@@ -3,13 +3,29 @@ const close = document.querySelector("#close");
 const nav = document.querySelector("#navbar");
 
 if(bar) {
-    bar.addEventListener("click", () => {
+    bar.addEventListener("click", function() {
         nav.classList.add("active");
     })
 }
 
 if(close) {
-    close.addEventListener("click", () => {
+    close.addEventListener("click", function() {
         nav.classList.remove("active");
     })
+}
+
+const mainImg = document.getElementById("mainImg");
+const smallImg = document.getElementsByClassName("smallImg");
+
+smallImg[0].onclick = function() {
+    mainImg.src = smallImg[0].src;
+}
+smallImg[1].onclick = function() {
+    mainImg.src = smallImg[1].src;
+}
+smallImg[2].onclick = function() {
+    mainImg.src = smallImg[2].src;
+}
+smallImg[3].onclick = function() {
+    mainImg.src = smallImg[3].src;
 }
